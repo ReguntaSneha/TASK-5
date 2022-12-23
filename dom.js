f6515dd221289fd862da22d6f9fedfa93ed4974e
ReguntaSneha <reguntasneha000@gmail.com>
2022-12-23 15:28:53 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+//GETELEMENTBYCLASSNAME
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor='blue';
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+items[3].style.backgroundColor='pink';
